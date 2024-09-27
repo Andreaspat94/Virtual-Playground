3324fd0087fb49d651e0cdb7aada9b8ef5cb8b70
AndreasPaterakis <andreas_pat@hotmail.gr>
2024-09-27 13:07:51 +0300

    Add ray interaction to all cubes

--- Assets/Scripts/CheckerManager.cs
+++ Assets/Scripts/CheckerManager.cs
@@ -527,12 +527,12 @@ public class CheckerManager : Singleton<CheckerManager>
     // This logic erases all cubes of the correct's answer color and replace it with the correct island
     public void MakeIsland(int id)
     {  
-        Debug.Log("MakeIsland called --> " + id);
+        // Debug.Log("MakeIsland called --> " + id);
         ResetTheCubeNumberOfColor(id);
         // 3) show good object
         
         CheckIfOK();
-        Debug.Log("ID[]-->" + startupTutorial.idOK[0] + startupTutorial.idOK[1]+ startupTutorial.idOK[2]+ startupTutorial.idOK[3]+ startupTutorial.idOK[4]+ startupTutorial.idOK[5]);
+        // Debug.Log("ID[]-->" + startupTutorial.idOK[0] + startupTutorial.idOK[1]+ startupTutorial.idOK[2]+ startupTutorial.idOK[3]+ startupTutorial.idOK[4]+ startupTutorial.idOK[5]);
         
         AudioManager.Instance.playSound("magic");
         MakeIslandNotInteractable(id);
@@ -869,7 +869,7 @@ public class CheckerManager : Singleton<CheckerManager>
     IEnumerator LetTimeForCubeToDrop()
     {
         startupTutorial.ActivateRayInteractors(false);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
         //activate hand grabs
         startupTutorial.ActivateRayInteractors(true);
     }
